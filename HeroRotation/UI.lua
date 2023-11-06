2df9cf592e6407ca1678d9a1496e48a0edeb89b7
Zekke <clement.dahi@gmail.com>
2023-11-06 15:15:13 +0100

    some maths

--- HeroRotation/UI.lua
+++ HeroRotation/UI.lua
@@ -902,6 +902,21 @@
 
   
 --- ======= External debugger Pixel =======
+  --b64 encoder
+  local b64chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+  local function encodeBase64(data)
+    return ((data:gsub('.', function(x) 
+        local r, b = '', x:byte()
+        for i = 8, 1, -1 do r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c = 0
+        for i = 1, 6 do c = c + (x:sub(i,i) == '1' and 2^(6-i) or 0) end
+        return b64chars:sub(c + 1, c + 1)
+    end)..({ '', '==', '=' })[#data % 3 + 1])
+  end
   -- Init
   function HR.PixelFrame:Init ()
     -- Frame Init
@@ -925,9 +940,10 @@
       SpellName = GetItemInfo(ID)
     end
     if SpellName and SpellName ~= "" then
-      local R = SpellName:byte(1)
-      local G = SpellName:byte(math.ceil(#SpellName / 2))
-      local B = SpellName:byte(#SpellName)
+      local SpellName = encodeBase64(SpellName)
+      local R = math.ceil(SpellName:byte(1) + SpellName:byte(#SpellName) + SpellName:byte(2)/20)
+      local G = SpellName:byte(math.ceil(#SpellName / 2)) + SpellName:byte(math.ceil(#SpellName / 2) + 1)
+      local B = math.ceil(SpellName:byte(2) + SpellName:byte(#SpellName - 1) + SpellName:byte(3)/20)
       --HR.Print(SpellName .. " #" .. string.format("%02X%02X%02X", R, G, B))
       R = R / 255
       G = G / 255
